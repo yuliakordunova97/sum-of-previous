66fc4e6de7edebbbb1132ecb01c8aa1fcd73674a
yuliakordunova97 <kordunovayulia@gmail.com>
2023-03-21 12:49:38 +0200

    added files

---
+++ src/main/java/com/epam/rd/autotasks/arrays/SumOfPrevious.java
@@ -0,0 +1,19 @@
+package com.epam.rd.autotasks.arrays;
+
+import java.util.Arrays;
+
+public class SumOfPrevious {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{1, -1, 0, 4, 6, 10, 15, 25};
+
+        System.out.println(Arrays.toString(getSumCheckArray(array)));
+    }
+
+    public static boolean[] getSumCheckArray(int[] array) {
+
+        boolean[] booleans = new boolean[array.length];
+
+        return booleans;
+    }
+}
